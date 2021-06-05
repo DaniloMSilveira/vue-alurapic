c2d9ddee85d087e4dec82fd10ba2fe90af7b921d
DaniloMSilveira <danilomunifafibe@hotmail.com>
2021-06-05 10:37:20 -0300

    Subindo projeto realizado no primeiro módulo

---
+++ alurapic/src/components/home/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input type="search" 
+      class="filtro" 
+      @input="filtro = $event.target.value" 
+      placeholder="filtre pelo título">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto.url">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao 
+            tipo="button" 
+            titulo="REMOVER"
+            :confirmacao="true"
+            estilo="perigo"
+            @botaoAtivado="remove(foto)"
+          />
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from '../shared/Painel/index.vue';
+import ImagemResponsiva from '../shared/ImagemResponsiva/index.vue';
+import Botao from '../shared/Botao/index.vue';
+
+export default {
+  components: {
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao' : Botao
+  },
+
+  data() {
+
+    return {
+
+      titulo: 'Alurapic', 
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  methods: {
+    remove(foto) {
+      alert('Remover a foto!' + foto.titulo);
+    }
+  },
+
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+
+</script>
+
+<style>
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
