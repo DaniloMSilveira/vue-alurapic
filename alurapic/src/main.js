6827c7092de0ebbdcaa04232ea4215c606aad5d4
DaniloMSilveira <danilomunifafibe@hotmail.com>
2021-06-10 22:38:52 -0300

    Subindo aulas validação de formulário e dicas/truques para o vue

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -4,6 +4,12 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 
+import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
@@ -11,6 +17,15 @@ Vue.use(VueRouter);
 const router = new VueRouter({ 
   routes ,
   mode: 'history'
+});
+
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
 });
 
 new Vue({
